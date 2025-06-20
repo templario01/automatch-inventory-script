8c141a2bf2e77c6af52b350ffa60b3985ec77e8a
victor benavente <templario0141@gmail.com>
2025-06-20 13:27:12 -0500

    chore: remove gulp dependency

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-const uglify = require("gulp-uglify-es").default;
-const { src, dest } = require("gulp");
-
-const minify = () => src("./cache/**/*.js").pipe(uglify()).pipe(dest("./dist"));
-
-exports.minify = minify;
